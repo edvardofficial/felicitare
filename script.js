83228304402e47344259fbe7e6aba5707ebe42f5
edvardofficial <102943223+edvardofficial@users.noreply.github.com>
2023-08-08 02:25:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function submitForm() {
     var password = document.querySelector('.password').value;
-    if (password == 'iloveyou') {
+    if (password == 'I love you') {
+      alert("I love you tooo❤️")
       window.location.href = "./page/page.html";
     } else {
       alert('Parola nu este corecta');
